fa5df6f1697918aa3cc04e81fb74996b187b0c0f
visklm <marius.kleiner@vector.com>
2024-03-19 14:39:40 +0100

    Add version check

--- CypWorkableOuterRings/Text/CypWor_Text.sql
+++ CypWorkableOuterRings/Text/CypWor_Text.sql
@@ -2,6 +2,41 @@
 --------------------------------------------------------------
 
 --------------------------------------------------------------
+-- Mod
+--------------------------------------------------------------
+-- LocalizedText
+INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
+-- Mod and Creator Name
+('en_US',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "Yaxchilan"),
+('de_DE',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "Yaxchilan"),
+('fr_FR',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "TODO"),
+('pt_BR',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "TODO"),
+('es_ES',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "TODO"),
+('ja_JP',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "TODO"),
+('zh_Hant_HK',  'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "TODO"),
+('zh_Hans_CN',  'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "TODO"),
+('en_US',       'LOC_CYP_WOR_MOD_NAME_COLOR',                             "[COLOR_FLOAT_FOOD]{LOC_CYP_WOR_MOD_NAME_SIMPLE}[ENDCOLOR]"),
+('en_US',       'LOC_CYP_WOR_MOD_CREATOR',                                "[COLOR_FLOAT_GOLD]Cyp[ENDCOLOR]Ryan"),
+-- Compatibility Note Message
+('en_US',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', '{LOC_CYP_WOR_MOD_NAME_SIMPLE} compatibility note'),
+('de_DE',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', '{LOC_CYP_WOR_MOD_NAME_SIMPLE} Kompatibilität Hinweis'),
+('fr_FR',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', 'TODO'),
+('pt_BR',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', 'TODO'),
+('es_ES',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', 'TODO'),
+('ja_JP',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', 'TODO'),
+('zh_Hant_HK',  'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', 'TODO'),
+('zh_Hans_CN',  'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', 'TODO'),
+-- Compatibility Note Summary
+('en_US',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'Your game has been started with another version of the mod {LOC_CYP_WOR_MOD_NAME_COLOR}. If this leads to bugs I am very sorry for that! ~{LOC_CYP_WOR_MOD_CREATOR}'),
+('de_DE',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'Das Spiel wurde mit einer anderen Version der Mod {LOC_CYP_WOR_MOD_NAME_COLOR} gestartet. Falls das zu Bugs während des Spiels für, möchte ich ich dafür entschuldigen! ~{LOC_CYP_WOR_MOD_CREATOR}'),
+('fr_FR',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'TODO'),
+('pt_BR',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'TODO'),
+('es_ES',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'TODO'),
+('ja_JP',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'TODO'),
+('zh_Hant_HK',  'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'TODO'),
+('zh_Hans_CN',  'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'TODO');
+
+--------------------------------------------------------------
 -- Ingame
 --------------------------------------------------------------
 -- LocalizedText
@@ -13,8 +48,8 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 ('de_DE',       'LOC_DISTRICT_CYP_WOR_DESCRIPTION',   "Bürger können Geländerfelder im vierten Ring dieser Stadt zu bearbeiten."),
 ('fr_FR',       'LOC_DISTRICT_CYP_WOR_NAME',          "Yaxchilan"), -- ??
 ('fr_FR',       'LOC_DISTRICT_CYP_WOR_DESCRIPTION',   "TODO"),
-('it_IT',       'LOC_DISTRICT_CYP_WOR_NAME',          "Yaxchilan"), -- ??
-('it_IT',       'LOC_DISTRICT_CYP_WOR_DESCRIPTION',   "TODO"),
+('pt_BR',       'LOC_DISTRICT_CYP_WOR_NAME',          "Yaxchilan"), -- ??
+('pt_BR',       'LOC_DISTRICT_CYP_WOR_DESCRIPTION',   "TODO"),
 ('es_ES',       'LOC_DISTRICT_CYP_WOR_NAME',          "Yaxchilan"), -- ??
 ('es_ES',       'LOC_DISTRICT_CYP_WOR_DESCRIPTION',   "TODO"),
 ('ja_JP',       'LOC_DISTRICT_CYP_WOR_NAME',          "TODO"), -- ??
@@ -26,7 +61,7 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 --ru_RU
 --pl_PL
 --ko_KR
---pt_BR
+--it_IT
 -- Building A - Customs House
 ('en_US',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_NAME',            "Customs House"),
 ('en_US',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_DESCRIPTION',     "Citizens can work tiles in the fifth ring of this city."),
@@ -34,8 +69,8 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 ('de_DE',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_DESCRIPTION',     "Bürger können Geländerfelder im fünften Ring dieser Stadt zu bearbeiten."),
 ('fr_FR',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_NAME',            "TODO"),
 ('fr_FR',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_DESCRIPTION',     "TODO"),
-('it_IT',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_NAME',            "TODO"),
-('it_IT',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_DESCRIPTION',     "TODO"),
+('pt_BR',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_NAME',            "TODO"),
+('pt_BR',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_DESCRIPTION',     "TODO"),
 ('es_ES',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_NAME',            "TODO"),
 ('es_ES',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_DESCRIPTION',     "TODO"),
 ('ja_JP',       'LOC_BUILDING_CYP_WOR_CUSTOMS_HOUSE_NAME',            "TODO"),
@@ -51,8 +86,8 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 ('de_DE',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "Geländerfelder im vierten und fünften Ring dieser Stadt können mit [ICON_Gold] Gold erworben werden."),
 ('fr_FR',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_NAME',         "TODO"),
 ('fr_FR',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "TODO"),
-('it_IT',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_NAME',         "TODO"),
-('it_IT',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "TODO"),
+('pt_BR',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_NAME',         "TODO"),
+('pt_BR',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "TODO"),
 ('es_ES',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_NAME',         "TODO"),
 ('es_ES',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "TODO"),
 ('ja_JP',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_NAME',         "TODO"),
@@ -71,7 +106,7 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 ('en_US',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('de_DE',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('fr_FR',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "TODO"),
-('it_IT',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "TODO"),
+('pt_BR',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('es_ES',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('ja_JP',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('zh_Hant_HK',  'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "TODO"),
@@ -80,7 +115,7 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 ('en_US',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_CUSTOMS_HOUSE_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('de_DE',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_CUSTOMS_HOUSE_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('fr_FR',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_CUSTOMS_HOUSE_CHAPTER_HISTORY_PARA_1', "TODO"),
-('it_IT',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_CUSTOMS_HOUSE_CHAPTER_HISTORY_PARA_1', "TODO"),
+('pt_BR',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_CUSTOMS_HOUSE_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('es_ES',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_CUSTOMS_HOUSE_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('ja_JP',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_CUSTOMS_HOUSE_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('zh_Hant_HK',  'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_CUSTOMS_HOUSE_CHAPTER_HISTORY_PARA_1', "TODO"),
@@ -89,7 +124,7 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 ('en_US',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('de_DE',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('fr_FR',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "TODO"),
-('it_IT',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "TODO"),
+('pt_BR',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('es_ES',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('ja_JP',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "TODO"),
 ('zh_Hant_HK',  'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "TODO"),
