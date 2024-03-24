97b095d28c48fc31d1ad11bc23b575fb2705cf29
CypRyan <cypryan@outlook.de>
2024-03-24 09:00:30 +0100

    Merge text files and add localized description to ingame mod view.

--- CypWorkableOuterRings/Text/CypWor_Text.sql
+++ CypWorkableOuterRings/Text/CypWor_Text.sql
@@ -11,20 +11,33 @@
 --------------------------------------------------------------
 -- LocalizedText
 INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
--- Mod and Creator Name
+-- Mod Name Simple
 ('en_US',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "Cyp's Workable Outer Rings"),
 ('de_DE',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "Cyp's Workable Outer Rings"),
 ('fr_FR',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "Les anneaux extérieurs exploitables de Cyp"),
 ('pt_BR',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "Anéis externos trabalháveis do Cyp"),
 ('es_ES',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "Anillos exteriores practicables de Cyp"),
 ('it_IT',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "Anelli esterni lavorabili di Cyp"),
+('ja_JP',       'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "???"), -- TODO
+('zh_Hant_HK',  'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "???"), -- TODO
+('zh_Hans_CN',  'LOC_CYP_WOR_MOD_NAME_SIMPLE',                            "开发外环"), -- TODO
+-- Mod Name
 ('en_US',       'LOC_CYP_WOR_MOD_NAME_COLOR',                             "[COLOR_FLOAT_GOLD]Cyp[ENDCOLOR]Ryan's Workable Outer Rings"),
 ('de_DE',       'LOC_CYP_WOR_MOD_NAME_COLOR',                             "[COLOR_FLOAT_GOLD]Cyp[ENDCOLOR]Ryan's Workable Outer Rings"),
 ('fr_FR',       'LOC_CYP_WOR_MOD_NAME_COLOR',                             "Les anneaux extérieurs exploitables de [COLOR_FLOAT_GOLD]Cyp[ENDCOLOR]Ryan"),
 ('pt_BR',       'LOC_CYP_WOR_MOD_NAME_COLOR',                             "Anéis externos trabalháveis do [COLOR_FLOAT_GOLD]Cyp[ENDCOLOR]Ryan"),
 ('es_ES',       'LOC_CYP_WOR_MOD_NAME_COLOR',                             "Anillos exteriores practicables de [COLOR_FLOAT_GOLD]Cyp[ENDCOLOR]Ryan"),
 ('it_IT',       'LOC_CYP_WOR_MOD_NAME_COLOR',                             "Anelli esterni lavorabili di [COLOR_FLOAT_GOLD]Cyp[ENDCOLOR]Ryan"),
+('ja_JP',       'LOC_CYP_WOR_MOD_NAME_COLOR',                             "???"), -- TODO
+('zh_Hant_HK',  'LOC_CYP_WOR_MOD_NAME_COLOR',                             "???"), -- TODO
+('zh_Hans_CN',  'LOC_CYP_WOR_MOD_NAME_COLOR',                             "开发外环"), -- TODO
+-- Mod Creator
 ('en_US',       'LOC_CYP_WOR_MOD_CREATOR',                                "[COLOR_FLOAT_GOLD]Cyp[ENDCOLOR]Ryan"),
+-- Mod Teaser & Description
+('en_US',       'LOC_CYP_WOR_MOD_DESCRIPTION',                            "Adds the [COLOR_GREEN]{LOC_DISTRICT_CYP_WOR_NAME}[ENDCOLOR] district that allows working tiles in the fourth and fifth city ring."),
+('ja_JP',       'LOC_CYP_WOR_MOD_DESCRIPTION',                            "???"), -- TODO
+('zh_Hant_HK',  'LOC_CYP_WOR_MOD_DESCRIPTION',                            "???"), -- TODO
+('zh_Hans_CN',  'LOC_CYP_WOR_MOD_DESCRIPTION',                            "新增允许在第四和第五城市圈使用工作牌的[COLOR_GREEN]{LOC_DISTRICT_CYP_WOR_NAME}[ENDCOLOR]地区。"),
 -- Compatibility Note Message
 ('en_US',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', '{LOC_CYP_WOR_MOD_NAME_SIMPLE} compatibility note'),
 ('de_DE',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', '{LOC_CYP_WOR_MOD_NAME_SIMPLE} Kompatibilität Hinweis'),
@@ -32,13 +45,19 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 ('pt_BR',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', '{LOC_CYP_WOR_MOD_NAME_SIMPLE} Nota de compatibilidade'),
 ('es_ES',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', '{LOC_CYP_WOR_MOD_NAME_SIMPLE} Nota de compatibilidad'),
 ('it_IT',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', '{LOC_CYP_WOR_MOD_NAME_SIMPLE} Nota di compatibilità'),
+('ja_JP',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', '{LOC_CYP_WOR_MOD_NAME_SIMPLE} 互換性メモ'),
+('zh_Hant_HK',  'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', '{LOC_CYP_WOR_MOD_NAME_SIMPLE} 適配性備註'),
+('zh_Hans_CN',  'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_MESSAGE', '{LOC_CYP_WOR_MOD_NAME_SIMPLE} 适配性备注'),
 -- Compatibility Note Summary
 ('en_US',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'Your game has been started with another version of the mod {LOC_CYP_WOR_MOD_NAME_COLOR}. If this leads to bugs I am very sorry for that! ~{LOC_CYP_WOR_MOD_CREATOR}'),
 ('de_DE',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'Das Spiel wurde mit einer anderen Version der Mod {LOC_CYP_WOR_MOD_NAME_COLOR} gestartet. Falls das zu Bugs während des Spiels für, möchte ich ich dafür entschuldigen! ~{LOC_CYP_WOR_MOD_CREATOR}'),
 ('fr_FR',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'Le jeu a été lancé avec une version différente du mod {LOC_CYP_WOR_MOD_NAME_COLOR}. Si cela entraîne des bugs pendant le jeu, je tiens à m’en excuser ! ~{LOC_CYP_WOR_MOD_CREATOR}'),
 ('pt_BR',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'O seu jogo foi iniciado com uma versão diferente do mod {LOC_CYP_WOR_MOD_NAME_COLOR}. Se isso levar a bugs, peço desculpas! ~{LOC_CYP_WOR_MOD_CREATOR}'),
 ('es_ES',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'Tu juego ha sido iniciado con otra versión del mod {LOC_CYP_WOR_MOD_NAME_COLOR}. Si esto conduce a errores, ¡lo siento mucho! ~{LOC_CYP_WOR_MOD_CREATOR}'),
-('it_IT',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'Il tuo gioco è stato avviato con una versione diversa del mod {LOC_CYP_WOR_MOD_NAME_COLOR}. Se questo porta a bug, mi dispiace molto! ~{LOC_CYP_WOR_MOD_CREATOR}');
+('it_IT',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'Il tuo gioco è stato avviato con una versione diversa del mod {LOC_CYP_WOR_MOD_NAME_COLOR}. Se questo porta a bug, mi dispiace molto! ~{LOC_CYP_WOR_MOD_CREATOR}'),
+('ja_JP',       'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', 'あなたのゲームは別のバージョンのMOD {LOC_CYP_WOR_MOD_NAME_COLOR}で開始されています。これがバグにつながるのであれば、大変申し訳ありません！~{LOC_CYP_WOR_MOD_CREATOR}'),
+('zh_Hant_HK',  'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', '您的遊戲以另一版本的{LOC_CYP_WOR_MOD_NAME_COLOR}啟動了。若這導致任何BUG則非常抱歉！~{LOC_CYP_WOR_MOD_CREATOR}'),
+('zh_Hans_CN',  'LOC_CYP_WOR_NOTIFICATION_VERSION_COMPATIBILITY_SUMMARY', '您的游戏以另一版本的{LOC_CYP_WOR_MOD_NAME_COLOR}启动了。若这导致任何BUG则非常抱歉！~{LOC_CYP_WOR_MOD_CREATOR}');
 
 --------------------------------------------------------------
 -- Ingame
@@ -58,13 +77,22 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 ('es_ES',       'LOC_DISTRICT_CYP_WOR_DESCRIPTION',   "Los ciudadanos pueden trabajar en las casillas del cuarto anillo de esta ciudad. Las casillas del cuarto anillo de esta ciudad se pueden comprar con Oro [ICON_GOLD]. Bombardea culturalmente las casillas neutrales adyacentes"),
 ('it_IT',       'LOC_DISTRICT_CYP_WOR_NAME',          "Quartieri della città di Frontiera"),
 ('it_IT',       'LOC_DISTRICT_CYP_WOR_DESCRIPTION',   "I cittadini possono lavorare le tessere nel quarto anello di questa città. Le tessere del quarto anello di questa città possono essere acquistate con [ICON_GOLD] Oro. Bombardea culturalmente las casillas neutrales adyacentes"),
--- District adjacency
+('ja_JP',       'LOC_DISTRICT_CYP_WOR_NAME',          "開拓地の町"),
+('ja_JP',       'LOC_DISTRICT_CYP_WOR_DESCRIPTION',   "市民は都市から4タイル以内のタイルを利用できる。この都市が [ICON_Gold] ゴールドで4タイル以内のタイルを購入できるようになる。所有者のいない隣接タイルに対して文化爆弾を発動させる。"),
+('zh_Hant_HK',  'LOC_DISTRICT_CYP_WOR_NAME',          "拓荒駐地"),
+('zh_Hant_HK',  'LOC_DISTRICT_CYP_WOR_DESCRIPTION',   "城市人口可以在此城市四環內工作。此城市可使用 [ICON_Gold] 金幣購買四環單元格。建成時觸發文化炸彈，對相鄰中立單元格施放文化炸彈。"),
+('zh_Hans_CN',  'LOC_DISTRICT_CYP_WOR_NAME',          "拓荒驻地"),
+('zh_Hans_CN',  'LOC_DISTRICT_CYP_WOR_DESCRIPTION',   "城市人口可以在此城市四环内工作。此城市可使用 [ICON_Gold] 金币购买四环单元格。建成时触发文化炸弹，对相邻中立单元格施放文化炸弹。"),
+-- District Adjacency
 ('en_US',       'LOC_DISTRICT_CYP_WOR_GOLD', '+{1_num} [ICON_Gold] Gold from the adjacent {LOC_DISTRICT_CYP_WOR_NAME} district.'),
 ('de_DE',       'LOC_DISTRICT_CYP_WOR_GOLD', '+{1_num} [ICON_Gold] Gold durch den angrenzenden {LOC_DISTRICT_CYP_WOR_NAME} Bezirk.'),
 ('fr_FR',       'LOC_DISTRICT_CYP_WOR_GOLD', '{LOC_DISTRICT_CYP_WOR_NAME} adjacent : [ICON_Gold] or +{1_num}.'),
 ('pt_BR',       'LOC_DISTRICT_CYP_WOR_GOLD', '+{1_num} de [ICON_Gold] ouro do distrito de {LOC_DISTRICT_CYP_WOR_NAME} adjacente.'),
 ('es_ES',       'LOC_DISTRICT_CYP_WOR_GOLD', '+{1_num} de Oro [ICON_Gold] por el distrito de {LOC_DISTRICT_CYP_WOR_NAME} adyacente.'),
 ('it_IT',       'LOC_DISTRICT_CYP_WOR_GOLD', '+{1_num} [ICON_Gold] Oro dal distretto {LOC_DISTRICT_CYP_WOR_NAME} adiacente.'),
+('ja_JP',       'LOC_DISTRICT_CYP_WOR_GOLD', '隣接する港区域により [ICON_Gold] ゴールド+{1_num}。'),
+('zh_Hant_HK',  'LOC_DISTRICT_CYP_WOR_GOLD', '+{1_num} [ICON_Gold] 金幣來自相鄰港口區域。'),
+('zh_Hans_CN',  'LOC_DISTRICT_CYP_WOR_GOLD', '+{1_num} [ICON_Gold] 金币来自相邻港口。'),
 -- Building - Customs House
 ('en_US',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_NAME',         "Logistics Center"),
 ('en_US',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "Citizens can work tiles in the fifth ring of this city. Tiles in the fifth ring of this city can be purchased by [ICON_GOLD] Gold. Railroad bombs adjacent owned tiles if the technology {LOC_TECH_STEAM_POWER_NAME} is researched at the time of completion."),
@@ -78,6 +106,12 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 ('es_ES',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "Los ciudadanos pueden trabajar en las casillas del quinto anillo de esta ciudad. Las casillas del quinto anillo de esta ciudad se pueden comprar con oro [ICON_GOLD]. Mejora los campos de terreno circundantes con vías férreas si la tecnología {LOC_TECH_STEAM_POWER_NAME} está investigada en el momento de la finalización."),
 ('it_IT',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_NAME',         "Centro Logistico"),
 ('it_IT',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "I cittadini possono lavorare le tessere nel quinto anello di questa città. Le tessere del quinto anello di questa città possono essere acquistate con [ICON_GOLD] Oro. Migliora i campi di terreno circostanti con binari ferroviari se la tecnologia {LOC_TECH_STEAM_POWER_NAME} è ricercata al momento del completamento.");
+('ja_JP',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_NAME',         "物流センター"),
+('ja_JP',       'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "市民は都市から5タイル以内のタイルを利用できる。この都市が [ICON_Gold] ゴールドで5タイル以内のタイルを購入できるようになる。完成時に「蒸気機関」技術が研究した後なら、1タイル以内のタイルに鉄道を作る。"),
+('zh_Hant_HK',  'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_NAME',         "物流中心"),
+('zh_Hant_HK',  'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "城市人口可以在此城市五環內工作。此城市可使用 [ICON_Gold] 金幣購買五環單元格。若建成時已研究「蒸汽動力」科技，則在1單元格內所有陸地單元格鋪設鐵路。"),
+('zh_Hans_CN',  'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_NAME',         "物流中心"),
+('zh_Hans_CN',  'LOC_BUILDING_CYP_WOR_LOGISTICS_CENTER_DESCRIPTION',  "城市人口可以在此城市五环内工作。此城市可使用 [ICON_Gold] 金币购买五环单元格。若建成时已研究“蒸汽动力”科技，则在1单元格内所有陆地单元格铺设铁路。");
 
 --------------------------------------------------------------
 -- Civilopedia
@@ -91,10 +125,16 @@ INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
 ('pt_BR',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "Ao longo da história, as civilizações sempre procuraram expandir seus territórios, empurrando as fronteiras de suas cidades para incorporar mais recursos e espaço. Os Bairros Fronteiriços são um testemunho desse aspecto constante da civilização humana. Ele representa a periferia de uma cidade, onde a natureza encontra o urbano, e onde o potencial bruto da terra não reivindicada é aproveitado para o crescimento da cidade."),
 ('es_ES',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "A lo largo de la historia, las civilizaciones siempre han buscado expandir sus territorios, empujando las fronteras de sus ciudades para incorporar más recursos y espacio. Los Barrios de la Frontera son un testimonio de este aspecto constante de la civilización humana. Representa la periferia de una ciudad, donde la naturaleza se encuentra con lo urbano, y donde el potencial bruto de la tierra no reclamada se aprovecha para el crecimiento de la ciudad."),
 ('it_IT',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "Nel corso della storia, le civiltà hanno sempre cercato di espandere i loro territori, spingendo i confini delle loro città per incorporare più risorse e spazio. I Frontier City Quarters sono una testimonianza di questo aspetto costante della civiltà umana. Rappresenta la periferia di una città, dove la natura incontra l'urbano, e dove il potenziale grezzo della terra non rivendicata viene sfruttato per la crescita della città."),
+('ja_JP',       'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "歴史を通じて、文明は常に領土を拡大しようとし、より多くの資源と空間を取り込むために都市の辺境を押し広げてきた。開拓地の町は、この人類文明の永続的な側面の証である。それは都市の近郊を表し、荒野と都市が交わる場所であり、未開拓の土地の純粋な可能性が都市の成長のために利用される場所なのだ。"),
+('zh_Hant_HK',  'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "縱觀歷史，人類文明一直在尋求擴張自己的領土，將城市的邊界推向更多的資源和空間。拓荒駐地是人類文明這一永恒主題的見證。它代表城市的外圍，是荒野與城市的交匯處，利用無主土地的潛力促進城市發展。"),
+('zh_Hans_CN',  'LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_CYP_WOR_CHAPTER_HISTORY_PARA_1', "纵观历史，人类文明一直在寻求扩张自己的领土，将城市的边界推向更多的资源和空间。拓荒驻地是人类文明这一永恒主题的见证。它代表城市的外围，是荒野与城市的交汇处，利用无主土地的潜力促进城市发展。"),
 -- Building - Logistics Center
 ('en_US',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "The concept of logistics has been integral to the growth and management of cities since ancient times. The Logistics Center represents the culmination of centuries of knowledge and experience in city planning and resource management. It symbolizes the civilization's ability to not just expand, but to effectively manage and sustain their growth through efficient logistics and planning."),
 ('de_DE',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "Das Konzept der Logistik ist seit der Antike integraler Bestandteil des Wachstums und der Verwaltung von Städten. Das Logistics Center repräsentiert die Krönung von Jahrhunderten des Wissens und der Erfahrung in der Stadtplanung und Ressourcenverwaltung. Es symbolisiert die Fähigkeit der Zivilisation, nicht nur zu expandieren, sondern ihr Wachstum durch effiziente Logistik und Planung effektiv zu verwalten und zu erhalten."),
 ('fr_FR',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "Le concept de logistique a été intégral à la croissance et à la gestion des villes depuis l'Antiquité. Le Logistics Center représente l'aboutissement de siècles de connaissances et d'expérience en matière de planification urbaine et de gestion des ressources. Il symbolise la capacité de la civilisation à non seulement s'étendre, mais à gérer et soutenir efficacement sa croissance grâce à une logistique et une planification efficaces."),
 ('pt_BR',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "O conceito de logística tem sido integral ao crescimento e à gestão das cidades desde a antiguidade. O Centro Logístico representa o ápice de séculos de conhecimento e experiência em planejamento urbano e gestão de recursos. Ele simboliza a capacidade da civilização de não apenas se expandir, mas de gerenciar e sustentar efetivamente seu crescimento por meio de uma logística e planejamento eficientes."),
 ('es_ES',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "El concepto de logística ha sido integral para el crecimiento y la gestión de las ciudades desde la antigüedad. El Centro Logístico representa la culminación de siglos de conocimiento y experiencia en planificación urbana y gestión de recursos. Simboliza la capacidad de la civilización no solo para expandirse, sino para gestionar y sostener eficazmente su crecimiento a través de una logística y planificación eficientes."),
-('it_IT',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "Il concetto di logistica è stato fondamentale per la crescita e la gestione delle città fin dall'antichità. Il Logistics Center rappresenta il culmine di secoli di conoscenza ed esperienza nella pianificazione urbana e nella gestione delle risorse. Simboleggia la capacità della civiltà non solo di espandersi, ma di gestire e sostenere efficacemente la sua crescita attraverso una logistica e una pianificazione efficienti.");
+('it_IT',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "Il concetto di logistica è stato fondamentale per la crescita e la gestione delle città fin dall'antichità. Il Logistics Center rappresenta il culmine di secoli di conoscenza ed esperienza nella pianificazione urbana e nella gestione delle risorse. Simboleggia la capacità della civiltà non solo di espandersi, ma di gestire e sostenere efficacemente la sua crescita attraverso una logistica e una pianificazione efficienti."),
+('ja_JP',       'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "物流という概念は、古来より都市の成長と管理に不可欠なものであった。物流センターは、都市計画と資源管理における何世紀にもわたる知識と経験の集大成である。それは、単に拡大するだけでなく、効率的な物流と計画によって成長を効果的に管理しながら維持する文明の能力を象徴している。"),
+('zh_Hant_HK',  'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "自古以來，物流的概念就與城市的發展和管理密不可分。物流中心是數世紀以來城市規劃和資源管理知識與經驗的結晶。它不僅象徵文明的擴張能力，還象徵通過高效的物流和規劃來有效管理和維持發展勢頭。"),
+('zh_Hans_CN',  'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_CYP_WOR_LOGISTICS_CENTER_CHAPTER_HISTORY_PARA_1', "自古以来，物流的概念就与城市的发展和管理密不可分。物流中心是数世纪以来城市规划和资源管理知识与经验的结晶。它不仅象徵文明的扩张能力，还象徵通过高效的物流和规划来有效管理和维持发展势头。");
--- CypWorkableOuterRings/Text/CypWor_Text_Yaxchilan.sql
+++ CypWorkableOuterRings/Text/CypWor_Text_Yaxchilan.sql
@@ -6,19 +6,15 @@
 --------------------------------------------------------------
 -- LocalizedText
 INSERT INTO "LocalizedText" ("Language", "Tag", "Text") VALUES
-('en_US', 'LOC_CYP_WOR_YAXCHILAN', "Yaxchilan");
+('en_US',       'LOC_CYP_WOR_YAXCHILAN', "Yaxchilan"),
+('ja_JP',       'LOC_CYP_WOR_YAXCHILAN', "ヤシュチラン"),
+('zh_Hant_HK',  'LOC_CYP_WOR_YAXCHILAN', "亞斯奇蘭"),
+('zh_Hans_CN',  'LOC_CYP_WOR_YAXCHILAN', "亚斯奇兰");
 -- LocalizedText (update)
---- en, de, ...
 UPDATE "LocalizedText"
-SET Text = 'Yaxchilan'
-WHERE Tag IN ('LOC_CYP_WOR_MOD_NAME_SIMPLE', 'LOC_DISTRICT_CYP_WOR_NAME')
-AND Language NOT IN ('zh_Hant_HK', 'zh_Hans_CN');
---- zh
-UPDATE "LocalizedText"
-SET Text = '亚斯奇兰'
-WHERE Tag IN ('LOC_CYP_WOR_MOD_NAME_SIMPLE', 'LOC_DISTRICT_CYP_WOR_NAME')
-AND Language IN ('zh_Hant_HK', 'zh_Hans_CN');
+SET Text = '{LOC_CYP_WOR_YAXCHILAN}'
+WHERE Tag IN ('LOC_CYP_WOR_MOD_NAME_SIMPLE', 'LOC_DISTRICT_CYP_WOR_NAME');
 -- colored
 UPDATE "LocalizedText"
-SET Text = '[COLOR_FLOAT_FOOD]{LOC_CYP_WOR_MOD_NAME_SIMPLE}[ENDCOLOR]'
+SET Text = '[COLOR_FLOAT_FOOD]{LOC_CYP_WOR_YAXCHILAN}[ENDCOLOR]'
 WHERE Tag IN ('LOC_CYP_WOR_MOD_NAME_COLOR');
